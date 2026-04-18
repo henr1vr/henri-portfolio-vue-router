3c032ff31d709f56eee14d608199a3e7f3efd2e8
henr1vr <henrivr77@gmail.com>
2026-04-18 15:26:00 +0200

    project card title position fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,25 +19,25 @@
     <!-- MY WORK SECTION -->
     <section id="mywork" class="mywork">
       <h2>My Work</h2>
-      <div class="Projects">
+            <div class="Projects">
         <div class="Project" @mouseenter="showProject(0)">
-          <span class="label yellow">W<br>o<br>o<br>d<br>W<br>o<br>r<br>X</span>
+          <span class="label yellow">WoodWorX</span>
           <img src="@/assets/images/Woodworx Preview image.png" alt="WoodWorX">
         </div>
         <div class="Project" @mouseenter="showProject(1)">
-          <span class="label orange">H<br>a<br>p<br>t<br>i<br>N<br>a<br>v</span>
+          <span class="label orange">HaptiNav</span>
           <img src="@/assets/images/Haptinav Preview image.png" alt="HaptiNav">
         </div>
         <div class="Project" @mouseenter="showProject(2)">
-          <span class="label dark-orange">I<br>l<br>l<br>t<br>h<br>a</span>
+          <span class="label dark-orange">Ilitha</span>
           <img src="@/assets/images/Ilitha Preview image.png" alt="Ilitha">
         </div>
         <div class="Project" @mouseenter="showProject(3)">
-          <span class="label purple">F<br>a<br><br>T<br>a<br>n<br>i<br>m<br>a<br>l<br>s</span>
+          <span class="label purple">Fa Tanimals</span>
           <img src="@/assets/images/fa tanimals Preview image.png" alt="Fa Tanimals">
         </div>
         <div class="Project" @mouseenter="showProject(4)">
-          <span class="label blue">E<br>x<br>p<br>l<br>o<br>r<br>a<br>t<br>i<br>o<br>n<br>s</span>
+          <span class="label blue">Explorations</span>
           <img src="@/assets/images/Explorations Preview image.png" alt="Explorations">
         </div>
       </div>
